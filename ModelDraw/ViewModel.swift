d062f8a9bcb240d993b7a75c2f0c403730a3a835
Michael Raftery <michael.raftery@terratrace.com>
2025-09-12 05:25:51 -0400

    ViewModel

--- ModelDraw/ViewModel.swift
+++ ModelDraw/ViewModel.swift
@@ -1,7 +1,19 @@
 //
-//  Untitled.swift
+//  ViewModel.swift
 //  ModelDraw
 //
 //  Created by Mike Raftery on 9/12/25.
 //
+/// The data that the app uses to configure its views.
 
+
+
+import SwiftUI
+import RealityKit
+
+
+@Observable
+class ViewModel {
+    
+    
+}
