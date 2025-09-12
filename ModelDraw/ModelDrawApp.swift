4cba87bf2dd69ab9a4b2d7c0502921c2fe95a883
Michael Raftery <michael.raftery@terratrace.com>
2025-09-12 05:13:42 -0400

    Initial Commit

---
+++ ModelDraw/ModelDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelDrawApp.swift
+//  ModelDraw
+//
+//  Created by Mike Raftery on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ModelDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
