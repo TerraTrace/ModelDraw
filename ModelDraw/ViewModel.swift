7e0d5e558cdf4a8cc9a9cc715f6582cbc58e1b41
Michael Raftery <michael.raftery@terratrace.com>
2025-09-14 14:33:53 -0400

    ViewModel

--- ModelDraw/ViewModel.swift
+++ ModelDraw/ViewModel.swift
@@ -15,5 +15,25 @@ import RealityKit
 @Observable
 class ViewModel {
     
+    // MARK: - Selection State
+    var selectedItem: SelectedItem?
+    
+    // MARK: - 3D Scene State
+    var cameraPosition: SIMD3<Float> = SIMD3<Float>(0, 2, 8)
+    var cameraRotation: simd_quatf = simd_quatf(angle: 0, axis: SIMD3<Float>(0, 1, 0))
+    
+    // MARK: - UI State
+    var showRedTestBox: Bool = true  // For debugging 3D scene
+    
+    // MARK: - Methods
+    func selectItem(_ item: SelectedItem?) {
+        selectedItem = item
+    }
+    
+    func rotateCamera(deltaX: Float, deltaY: Float) {
+        let sensitivity: Float = 0.01
+        cameraRotation = cameraRotation * simd_quatf(angle: -deltaX * sensitivity, axis: SIMD3(0, 1, 0))
+        cameraRotation = cameraRotation * simd_quatf(angle: deltaY * sensitivity, axis: SIMD3(1, 0, 0))
+    }
     
 }
